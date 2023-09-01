3bb9dcaf6541dedd14e484f2af6f0125987ea567
Joaolluiz <joao.fdk8@gmail.com>
2023-09-01 20:49:18 -0300

    Refactoring PersonController

--- library/src/main/java/com/webservice/library/controllers/PersonController.java
+++ library/src/main/java/com/webservice/library/controllers/PersonController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,38 +25,32 @@ public class PersonController {
 	@Autowired
 	private PersonServices personServices;
 	
-	
-	
-	@RequestMapping( method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) //O Swagger exige esse campo
 	public List<Person> findAll() {
 		return personServices.findAll();
 	}
 	
-	@RequestMapping(value = "/{id}", 
-			method = RequestMethod.GET
-			, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") Long id) {
 		return personServices.findById(id);
 	}
 	
-	@RequestMapping(value = "/{id}", 
-			method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") Long id) {
-		personServices.deletePerson(id);
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person create(@RequestBody Person person) {
 		return personServices.createPerson(person);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT, 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person update(@RequestBody Person person) {
 		return personServices.updatePerson(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		personServices.deletePerson(id);
+		return ResponseEntity.noContent().build();
 	}
 }
