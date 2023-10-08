53ce9fff89ed6cdeb2d595fae689cb62b08e85bb
Joaolluiz <joao.fdk8@gmail.com>
2023-10-08 19:32:19 -0300

    Customização da doc Swagger para o EndPoint FindAll

--- library/src/main/java/com/webservice/library/controllers/PersonController.java
+++ library/src/main/java/com/webservice/library/controllers/PersonController.java
@@ -17,15 +17,40 @@ import com.webservice.library.data.vo.v1.PersonVO;
 import com.webservice.library.services.PersonServices;
 import com.webservice.library.util.MediaType;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController {
 
 	@Autowired
 	private PersonServices personServices;
 	
+	//O Swagger exige esse campo
 	@GetMapping(
-		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })  //O Swagger exige esse campo
+		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Finds all People", description = "Finds all People",
+		tags = {"People"}, 
+		responses = {
+				@ApiResponse(description = "Success", responseCode = "200",
+					content = {
+						@Content(
+							mediaType = "application/json",
+							array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))	
+						)
+					}),
+				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+		}
+	)
 	public List<PersonVO> findAll() {
 		return personServices.findAll();
 	}
