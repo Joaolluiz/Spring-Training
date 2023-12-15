84138da7fc597f36eb950e6d8f09f66c856f60fe
Joaolluiz <joao.fdk8@gmail.com>
2023-12-15 20:44:02 -0300

    Implementando a classe JwtTokenProvider

--- library/src/main/java/com/webservice/library/entities/User.java
+++ library/src/main/java/com/webservice/library/entities/User.java
@@ -44,6 +44,7 @@ public class User implements UserDetails, Serializable {
 	@JoinTable(name = "user_permission", joinColumns = {@JoinColumn (name = "id_user")},
 		inverseJoinColumns = {@JoinColumn (name = "id_permission")}
 	)
+
 	private List<Permission> permissions;
 
 	public List<String> getRoles() {
--- library/src/main/java/com/webservice/library/services/UserServices.java
+++ library/src/main/java/com/webservice/library/services/UserServices.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
-
-
 @Service
 public class UserServices implements UserDetailsService {
 
