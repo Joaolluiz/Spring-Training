289016723070155ecfbdbc5515488322eceb1f23
Joaolluiz <joao.fdk8@gmail.com>
2024-02-05 23:08:59 +0000

    test: controller de person com XML

--- library/src/test/java/com/webservice/library/integrationtests/vo/TokenVO.java
+++ library/src/test/java/com/webservice/library/integrationtests/vo/TokenVO.java
@@ -5,7 +5,7 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
-@XmlRootElement
+@XmlRootElement(name = "TokenVO")
 public class TokenVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
