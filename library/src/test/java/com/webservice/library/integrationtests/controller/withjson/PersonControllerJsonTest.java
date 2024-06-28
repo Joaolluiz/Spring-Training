913cf834fdd99471364c2efa3499b0d6bfbc6caa
Joaolluiz <joao.fdk8@gmail.com>
2024-06-27 21:10:51 -0400

    test: fix nos testes de integração de Hateoas

--- library/src/test/java/com/webservice/library/integrationtests/controller/withjson/PersonControllerJsonTest.java
+++ library/src/test/java/com/webservice/library/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -339,6 +339,34 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest {
                 .statusCode(403);
     }
 
+    @Test
+    @Order(9)
+    public void testHateoas() throws JsonMappingException, JsonProcessingException {
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .queryParams("page", 3, "size", 10, "direction", "desc")
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        assertTrue(content.contains("_links\":{\"self\":{\"href\":\"http://localhost:8888/api/person/v1/404\"}}}"));
+        assertTrue(content.contains("_links\":{\"self\":{\"href\":\"http://localhost:8888/api/person/v1/274\"}}}"));
+        assertTrue(content.contains("_links\":{\"self\":{\"href\":\"http://localhost:8888/api/person/v1/785\"}}}"));
+
+        assertTrue(content.contains("prev\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=2&size=10&sort=firstName,desc\"}"));
+        assertTrue(content.contains("self\":{\"href\":\"http://localhost:8888/api/person/v1?page=3&size=10&direction=asc\"}"));
+        assertTrue(content.contains("next\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=4&size=10&sort=firstName,desc\""));
+        assertTrue(content.contains("last\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=100&size=10&sort=firstName,desc\"}}"));
+        assertTrue(content.contains("\"page\":{\"size\":10,\"totalElements\":1006,\"totalPages\":101,\"number\":3}}"));
+
+
+
+    }
 
     private void mockPerson() {
         person.setFirstName("Nelson");
