48f6d663aa099faaf039d277745ca1bbee03f2ec
Joaolluiz <joao.fdk8@gmail.com>
2024-11-19 14:15:44 -0400

    feat: Definindo Configurações de Upload e Download | Definindo Exceptions e Response | Criando o Serviço e o Controller responsável pelo upload de arquivos

---
+++ library/src/main/java/com/webservice/library/controllers/FileController.java
@@ -0,0 +1,37 @@
+package com.webservice.library.controllers;
+
+import com.webservice.library.data.vo.v1.UploadFileResponseVO;
+import com.webservice.library.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    private Logger logger = Logger.getLogger(FileController.class.getName());
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+        logger.info("Storing file to disk");
+        String fileName = fileStorageService.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+        return new UploadFileResponseVO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+}
